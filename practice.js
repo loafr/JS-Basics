eff11556e988187dbbcdbfa8965946f9588f8230
lrobins <lrobins@adobe.com>
2015-01-14 19:44:02 -0700

    completed the 'story'  problems

--- practice.js
+++ practice.js
@@ -5,7 +5,7 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-var isTyler = function (name) {
+var isTyler = function(name) {
   if(name == "Tyler") {
     return true;
   }
@@ -22,7 +22,7 @@ var isTyler = function (name) {
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  var getName = function () {
+  var getName = function() {
     var name = prompt("What is your name?")
     return name;
   }
@@ -35,7 +35,10 @@ var isTyler = function (name) {
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+  var welcome = function() {
+    var user = getName();
+    alert("Welcome, " + user);
+  }
 
 
 //Next problem
@@ -47,7 +50,13 @@ var isTyler = function (name) {
 //Save the result of the return value from your adder function, then alert "The Total Number was " + the number that was
 //returned from adder.
 
-  //Code Here
+var adder = function(arguments) {
+  var sum = arguments + arguments;
+  return sum;
+}
+
+var adderSum = adder();
+alert("The Total Number was " + adderSum);
 
 
 //Next Problem
@@ -56,7 +65,8 @@ var isTyler = function (name) {
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  //Arguments are passed in the invocation of the function -->  someFunction("Argument1, Argument2");   
+  //Parameters are pass in the definition of the functio  --> var someFunction(param1, param2) { do something };
 
 
 //Next problem
@@ -66,7 +76,13 @@ var isTyler = function (name) {
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  //falsy values = undefined, null, NaN, 0, "", false;
+  // you can check with an if statement to see if it is defined or exists:
+  /* if(name) {
+    return true;
+  } else {
+  return false;
+  }
 
 
 
@@ -76,7 +92,7 @@ var isTyler = function (name) {
 
 //Create a function called myName that returns your name
 
-  //Code Here
+  
 
 //Now save the function definition of myName into a new variable called newMyName
 
